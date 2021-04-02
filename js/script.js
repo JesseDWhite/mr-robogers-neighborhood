ecb2191c0cee91ac919057cca577ce734ae9b2e6
Jesse White <jesse.white6@gmail.com>
2021-04-02 09:20:10 -0700

    pass multiple inputs test for 3 2 1

--- js/script.js
+++ js/script.js
@@ -6,17 +6,17 @@ const numbersArray = ["1", "2", "3"]
 
 // ------------------------------------business logic-----------------------------------------------
 function beepBoop(number) {
-    const beepBoopArray = parseInt(number.split(" "));
+    const beepBoopArray = number.split(" ");
     beepBoopArray.forEach(function (element) {
         for (let i = 0; i < beepBoopArray.length; i++)
-            if (beepBoopArray === 3) {
+            if (beepBoopArray[2] === numbersArray[i]) {
                 console.log("Won't you be my neighbor?");
-            } else if (beepBoopArray === 2) {
+            } else if (beepBoopArray[1] === numbersArray[i]) {
                 console.log("Boop!");
-            } else if (beepBoopArray === 1) {
+            } else if (beepBoopArray[0] === numbersArray[i]) {
                 console.log("Beep!");
             } else {
-                console.log("That didn't work...");
+                console.log(beepBoopArray);
             }
     })
 }

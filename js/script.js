0a4c3e28c30d728f440402b1c7e70494c6c69b5d
Jesse White <jesse.white6@gmail.com>
2021-04-02 09:28:42 -0700

    remove forEach method and replace with for let.

--- js/script.js
+++ js/script.js
@@ -7,18 +7,16 @@ const numbersArray = ["1", "2", "3"]
 // ------------------------------------business logic-----------------------------------------------
 function beepBoop(number) {
     const beepBoopArray = number.split(" ");
-    beepBoopArray.forEach(function (element) {
-        for (let i = 0; i < beepBoopArray.length; i++)
-            if (beepBoopArray[2] === numbersArray[i]) {
-                console.log("Won't you be my neighbor?");
-            } else if (beepBoopArray[1] === numbersArray[i]) {
-                console.log("Boop!");
-            } else if (beepBoopArray[0] === numbersArray[i]) {
-                console.log("Beep!");
-            } else {
-                console.log(beepBoopArray);
-            }
-    })
+    for (let i = 0; i < beepBoopArray.length; i++)
+        if (beepBoopArray[2] === numbersArray[i]) {
+            console.log("Won't you be my neighbor?");
+        } else if (beepBoopArray[1] === numbersArray[i]) {
+            console.log("Boop!");
+        } else if (beepBoopArray[0] === numbersArray[i]) {
+            console.log("Beep!");
+        } else {
+            console.log(beepBoopArray);
+        }
 }
 
 

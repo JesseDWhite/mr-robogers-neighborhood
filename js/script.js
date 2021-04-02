8e0b9ec7841c1826e975f44ca191407348595ce6
Jesse White <jesse.white6@gmail.com>
2021-04-02 08:35:23 -0700

    pass initial beepBoop test.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 // ------------------------------------utility logic------------------------------------------------
+const robogersArray = ["Beep!", "Boop", "Won't you be my neighbor?"]
 
+const numbersArray = ["1", "2", "3"]
 
 
 // ------------------------------------business logic-----------------------------------------------
 function beepBoop(number) {
-    let beepBoopArray = number.split(" ")
+    const beepBoopArray = number.split(" ");
+    console.log(beepBoopArray)
 
 }
 
 
 // ------------------------------------user interface logic-----------------------------------------
 $(document).ready(function () {
-    $(form).submit(function (event) {
+    $("form").submit(function (event) {
         event.preventDefault()
 
     })

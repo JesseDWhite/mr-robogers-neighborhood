e1b90367853e964c00980b122e9e5ceda02216d8
Jesse White <jesse.white6@gmail.com>
2021-04-03 14:08:58 -0700

    update README.

--- README.md
+++ README.md
@@ -1,27 +1,26 @@
-# _{Application Name}_
-#### _{Brief description of application}_
-#### By _**{List of contributors}**
+# _mr-robogers-neighborhood_
+#### _Talk with Mr. Roboger and see if he will let you move in._
+#### By _Jesse White_
 ## Technologies Used
-* _List all_
-* _the major technologies_
-* _you used in your project_
-* _here
+* _HTML_
+* _CSS_
+* _Javascript_
+* _Bootstrap_
 ## Description
-_{This is a detailed description of your application. Give as much detail as needed to explain what the application does as well as any other information you want users or other developers to have.}
+_Mr. Roboger runs a great neighborhood, but he doesn't just let anyone in. Go ahead and start a converstation with him and see if he will let you move in! He will only respond to 1, 2, and 3, so be specific._
 ## Setup/Installation Requirements
-* _This is a great place_
-* _to list setup instructions_
-* _in a simple_
+* _Download all files from this repository_
+* _Open index.html in your browswer once it is downloaded_
+* _You will then have an empty form field that you can as Mr. Roboger a question_
 * _easy-to-understand_
 * _format_
-_{Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. Do I need to run a server? How should I set up my databases? Is there other code this application depends on? We recommend deleting the project from your desktop, re-cloning the project from GitHub, and writing down all the steps necessary to get the project working again.}_
 ## Known Bugs
-* _Any known issues_
+* _Mr. Roboger's response does not show up in the poper section._
 * _should go here_
 ## License
-_{Let people know what to do if they run into any issues or have questions, ideas or concerns.  Encourage them to contact you or make a contribution to the code.}_
+_MIT (C) Jesse White 2021_
 ## Contact Information
-_{Add your contact information here.}_
+Jesse White - _jesse.white6@gmail.com_
 ## Specs
 
 Describe beepBoop()
--- js/script.js
+++ js/script.js
@@ -1,61 +1,105 @@
 // ------------------------------------utility logic------------------------------------------------
 const robogerResponse = ["WON'T YOU BE MY NEIGHBOR?", "BOOP!", "BEEP!"]
 
+const robogersNumbers = ["3", "2", "1"]
+
 // ------------------------------------business logic-----------------------------------------------
 function beepBoop(number) {
-    if (number === " ") {
-        return null;
-    }
-    const beepBoopArray = number.split(" ");
-    let responseArray = [];
+    const beepBoopArray = number.split(" ")
+    let responseArray = []
     beepBoopArray.forEach(function (element) {
-        if (element.includes(3)) {
-            responseArray.push(robogerResponse[0]);
-            // console.log("I FOUND A 3!")
-            // console.log(responseArray)
-        } else if (element.includes(2)) {
-            responseArray.push(robogerResponse[1]);
-            // console.log("I FOUND A 2!")
-            // console.log(responseArray)
-        } else if (element.includes(1)) {
-            responseArray.push(robogerResponse[2]);
-            // console.log("I FOUND A 1!")
-            // console.log(responseArray)
-        } else {
-            responseArray.push(element);
-            // console.log(responseArray)
-        }
-        return responseArray.toString();
-    });
-};
+        for (let i = 0; i < beepBoopArray.length; i++) {
+            if (beepBoopArray[i] === robogersNumbers[0]) {
+                responseArray.push(robogerResponse[0]);
+
+
+            } else if (beepBoopArray[i] === robogersNumbers[1]) {
+                responseArray.push(robogerResponse[1]);
+
+
+            } else if (beepBoopArray[i] === robogersNumbers[2]) {
+                responseArray.push(robogerResponse[2]);
+
+
+            } else {
+                responseArray.push(element);
+            }
+        } return console.log(responseArray.toString());
+
+    })
+}
+
+
+
 // function beepBoop(number) {
-//     if (noInputtedWord(number)) {
-//         return null
+//     const beepBoopArray = number.split("");
+//     let responseArray = [];
+//     for (let i = 0; i < beepBoopArray.length; i++) {
+//         if (beepBoopArray[i] === robogersNumbers[0]) {
+//             responseArray.push(robogerResponse[0])
+//         } else if (beepBoopArray[i] === robogersNumbers[1]) {
+//             responseArray.push(robogerResponse[1]);
+//         } else if (beepBoopArray[i] === robogersNumbers[2]) {
+//             responseArray.push(robogerResponse[2])
+//         } else {
+//             responseArray.push(beepBoopArray[i])
+//         } return responseArray
+//     }
+// }
+
+
+// function beepBoop(number) {
+//     if (number === " ") {
+//         return null;
 //     }
 //     const beepBoopArray = number.split(" ");
+//     let responseArray = [];
+//     beepBoopArray.forEach(function (element) {
+//         if (element.includes(3)) {
+//             responseArray.push(robogerResponse[0]);
+//             // console.log("I FOUND A 3!")
+//             // console.log(responseArray)
+//         } else if (element.includes(2)) {
+//             responseArray.push(robogerResponse[1]);
+//             // console.log("I FOUND A 2!")
+//             // console.log(responseArray)
+//         } else if (element.includes(1)) {
+//             responseArray.push(robogerResponse[2]);
+//             // console.log("I FOUND A 1!")
+//             // console.log(responseArray)
+//         } else {
+//             responseArray.push(element);
+//             // console.log(responseArray)
+//         }
+//         return responseArray.toString();
+//     });
+// };
+
+
+// function beepBoop(number) {
+//     if (number === " ") {
+//         return null;
+//     }
+//     const beepBoopArray = number.toString().split(" ");
 //     let responseArray = []
 //     for (let i = 0; i < beepBoopArray.length; i++) {
-//         let convertResponseArr = number.toString()
-//         if (convertResponseArr.includes(3)) {
-//             responseArray.push(" WON'T YOU BE MY NEIGHBOR?");
+//         if (beepBoopArray[i] === robogersNumbers[0]) {
+//             responseArray.push("WON'T YOU BE MY NEIGHBOR?");
 //             console.log(beepBoopArray)
-//             console.log(convertResponseArr)
 //             console.log(responseArray)
 
-//         } else if (convertResponseArr.includes(2)) {
-//             responseArray.push(" BOOP!");
+//         } else if (beepBoopArray[i] === robogersNumbers[1]) {
+//             responseArray.push("BOOP!");
 //             console.log(beepBoopArray)
-//             console.log(convertResponseArr)
 //             console.log(responseArray)
 
-//         } else if (convertResponseArr.includes(1)) {
-//             responseArray.push(" BEEP!");
+//         } else if (beepBoopArray[i] === robogersNumbers[2]) {
+//             responseArray.push("BEEP!");
 //             console.log(beepBoopArray)
-//             console.log(convertResponseArr)
 //             console.log(responseArray)
 
 //         } else {
-//             responseArray.push(convertResponseArr);
+//             responseArray.push(number);
 //         }
 //     } return responseArray;
 // }
@@ -64,10 +108,12 @@ function beepBoop(number) {
 $(document).ready(function () {
     $("form").submit(function (event) {
         event.preventDefault();
-        const robogersResponse = $("#user-input").val();
 
-        $("#response").text(beepBoop(robogersResponse));
-        // $("#response").fadeIn(response);
+        const userInput = $("#user-input").val();
+        const robogersResponse = beepBoop(userInput)
+
+        $("#response").text(robogersResponse);
+        $("#response").fadeIn(response);
         // $("#response").fadeOut(response)
     });
 });
